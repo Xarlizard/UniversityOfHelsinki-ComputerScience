766d4a3317b500fcf1a8b361a11894c95142f554
xarlizard <charkrios@gmail.com>
2024-04-21 23:34:43 +0200

    Exercise 3.12 completed ✅

---
+++ FullStackOpen/Part-03/commandline_database/README.md
@@ -0,0 +1,18 @@
+# Exercise information
+
+Fullstack Open submission for Exercise 3.12 .
+
+Simple web applicaton for understanding the core concepts of MongoDB.
+
+## Start the application
+
+To start the application, do the following:
+
+```bash
+# Install dependancies
+$ npm install
+# Start the application
+$ npm start {password} {name} {number}
+```
+
+You can ommit `name` and `number` fields, and by doing so, the whole `phonebook persons` database will be displayed
---
+++ FullStackOpen/Part-03/commandline_database/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const nameField = process.argv[3]
+const numField = process.argv[4]
+
+const url =
+  `mongodb+srv://thecharkrios:${password}@cluster0.ryuajxr.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(nameField == undefined) {
+  
+  console.log("phonebook:")
+  Person.find({})
+    .then(result => {
+      result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+
+}else{
+
+  const person = new Person({
+    name: nameField,
+    number: numField,
+  })
+  
+  person.save().then(result => {
+    console.log(`added ${nameField} number ${numField} to phonebook`)
+    mongoose.connection.close()
+  })
+}
