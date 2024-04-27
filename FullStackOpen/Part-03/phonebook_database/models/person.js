6f2c53cbb7a464a9600548b0a2061b60fef1bf6b
xarlizard <charkrios@gmail.com>
2024-04-27 12:26:12 +0200

    Exercise 3.20 completed ✅

--- FullStackOpen/Part-03/phonebook_database/models/person.js
+++ FullStackOpen/Part-03/phonebook_database/models/person.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose')
-
 mongoose.set('strictQuery', false)
 
-
 const url = process.env.MONGODB_URI
-
-
 console.log('connecting to', url)
 
-mongoose.connect(url)
 
+mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB')
   })
@@ -23,7 +19,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
